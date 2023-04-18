1590c0457de9f0c99fc90b68bb0ab92956b86651
alanlunardhi <129990491+alanlunardhi@users.noreply.github.com>
2023-04-18 14:50:00 -0700

    Add files via upload
    added for loop to test different betas and plot PI, PAbar, Pabar, and Pv

--- lung.m
+++ lung.m
@@ -2,6 +2,15 @@
 clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
-setup_lung
-cvsolve
-outchecklung
+for beta = 0:0.1:1
+    setup_lung
+    cvsolve
+    outchecklung
+    figure(100)
+    plot(beta,PI,'r.')
+    hold on
+    plot(beta,PAbar,'g.')
+    plot(beta,Pabar,'b.')
+    plot(beta,Pv,'k.')
+end
+legend('PI', 'PAbar', 'Pabar', 'Pv')
