07992a6667aea12d059c9e373cce90813a2fbd97
alanlunardhi <129990491+alanlunardhi@users.noreply.github.com>
2023-04-19 21:27:47 -0700

    Update lung.m
    task 5 commit: plotting partial pressures and oxygen concentrations as functions of cI

--- lung.m
+++ lung.m
@@ -2,15 +2,43 @@
 clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
-for beta = 0:0.1:1
+%must comment cI out of setup_lung.m and uncomment beta value for this script to work
+cImat = []
+PAbarmat = []
+Pabarmat = []
+Pvmat = []
+cAbarmat = []
+cabarmat = []
+cvmat = []
+for cI = (0.1:0.01:0.2)/(22.4*(310/273)) 
     setup_lung
     cvsolve
     outchecklung
-    figure(100)
-    plot(beta,PI,'r.')
-    hold on
-    plot(beta,PAbar,'g.')
-    plot(beta,Pabar,'b.')
-    plot(beta,Pv,'k.')
+    cImat = [cImat cI]
+    PAbarmat = [PAbarmat PAbar]
+    Pabarmat = [Pabarmat Pabar]
+    Pvmat = [Pvmat Pv]
+    cAbarmat = [cAbarmat cAbar]
+    cabarmat = [cabarmat cabar]
+    cvmat = [cvmat cv]
 end
-legend('PI', 'PAbar', 'Pabar', 'Pv')
+
+figure(100)
+plot(cImat,PAbarmat,'g.', 'Markersize',24)
+hold on
+plot(cImat,Pabarmat,'b.', 'Markersize',24)
+plot(cImat,Pvmat,'k.', 'Markersize',24)
+xlabel('cI')
+ylabel('Partial Pressure')
+title('Various Partial Pressures vs cI')
+legend('PAbar - mean alveolar partial pressure of oxygen', 'Pabar - mean arterial partial pressure of oxygen', 'Pv - venous partial pressure of oxygen')
+
+figure(200)
+plot(cImat,cAbarmat,'g.', 'Markersize',24)
+hold on
+plot(cImat,cabarmat,'b.', 'Markersize',24)
+plot(cImat,cv,'k.', 'Markersize',24)
+xlabel('cI')
+ylabel('Oxygen Concentration')
+title('Various oxygen concentrations vs cI')
+legend('cAbar - mean alveolar concentration of oxygen', 'cabar - mean arterial concentration of oxygen', 'cv - venous concentration of oxygen')
