78846b61169b03e2894241de44b5d268f0e8554b
alanlunardhi <129990491+alanlunardhi@users.noreply.github.com>
2023-04-22 20:16:51 -0700

    Update lung.m
    task 10 commit

--- lung.m
+++ lung.m
@@ -1,53 +1,47 @@
 %filename: lung.m (main program)
 clear all
 clf
-cref=0.2/(22.4*(310/273))
-
 global Pstar cstar n maxcount M Q camax RT cI;
-for beta = 0:0.25:1
-    cstarmat = []
-    cImat = []
-    PImat = []
-    PAbarmat = []
-    Pabarmat = []
-    Pvmat = []
-    cAbarmat = []
-    cabarmat = []
-    cvmat = []
-    for cstar = cref*(1:-0.1:0.5)
-        setup_lung
-        cvsolve
-        outchecklung
-        cstarmat = [cstarmat cstar]
-        cImat = [cImat cI]
-        PImat = [PImat PI]
-        PAbarmat = [PAbarmat PAbar]
-        Pabarmat = [Pabarmat Pabar]
-        Pvmat = [Pvmat Pv]
-        cAbarmat = [cAbarmat cAbar]
-        cabarmat = [cabarmat cabar]
-        cvmat = [cvmat cv]
-    end
-
-    figure
-    plot(cstarmat,PImat,'r.','MarkerSize',24)
-    hold on
-    plot(cstarmat,PAbarmat,'g.', 'Markersize',24)
-    plot(cstarmat,Pabarmat,'b.', 'Markersize',24)
-    plot(cstarmat,Pvmat,'k.', 'Markersize',24)
-    xlabel('cstar')
-    ylabel('Partial Pressure')
-    title(sprintf('Various Partial Pressures vs cstar at beta = %d',beta))
-    legend('PI - oxygen partial pressure in inhaled air', 'PAbar - mean alveolar partial pressure of oxygen', 'Pabar - mean arterial partial pressure of oxygen', 'Pv - venous partial pressure of oxygen')
-    
-    figure
-    plot(cstarmat,cImat,'r.','MarkerSize',24)
-    hold on
-    plot(cstarmat,cAbarmat,'g.', 'Markersize',24)
-    plot(cstarmat,cabarmat,'b.', 'Markersize',24)
-    plot(cstarmat,cvmat,'k.', 'Markersize',24)
-    xlabel('cstar')
-    ylabel('Oxygen Concentration')
-    title(sprintf('Various oxygen concentrations vs cstar at beta = %d',beta))
-    legend('cI - inspired air concentration of oxygen','cAbar - mean alveolar concentration of oxygen', 'cabar - mean arterial concentration of oxygen', 'cv - venous concentration of oxygen')
+cImat = []
+PAbarmat = []
+Pabarmat = []
+Pvmat = []
+cAbarmat = []
+cabarmat = []
+cvmat = []
+cref=0.2/(22.4*(310/273))
+cstar = 0.5*cref
+for cI = (0.1:0.01:0.2)/(22.4*(310/273)) 
+    setup_lung
+    cvsolve
+    outchecklung
+    cImat = [cImat cI]
+    PAbarmat = [PAbarmat PAbar]
+    Pabarmat = [Pabarmat Pabar]
+    Pvmat = [Pvmat Pv]
+    cAbarmat = [cAbarmat cAbar]
+    cabarmat = [cabarmat cabar]
+    cvmat = [cvmat cv]
 end
+
+altitudes = linspace(0,19000,length(cImat));
+
+figure(300)
+plot(altitudes,PAbarmat(end:-1:1),'g.', 'Markersize',24)
+hold on
+plot(altitudes,Pabarmat(end:-1:1),'b.', 'Markersize',24)
+plot(altitudes,Pvmat(end:-1:1),'k.', 'Markersize',24)
+xlabel('Altitudes (feet)')
+ylabel('Partial Pressure')
+title('Various Partial Pressures vs Altitude for an Anemic with cstar = 0.5*cref')
+legend('PAbar - mean alveolar partial pressure of oxygen', 'Pabar - mean arterial partial pressure of oxygen', 'Pv - venous partial pressure of oxygen')
+
+figure(400)
+plot(altitudes,cAbarmat(end:-1:1),'g.', 'Markersize',24)
+hold on
+plot(altitudes,cabarmat(end:-1:1),'b.', 'Markersize',24)
+plot(altitudes,cvmat(end:-1:1),'k.', 'Markersize',24)
+xlabel('Altitude (feet)')
+ylabel('Oxygen Concentration')
+title('Various oxygen concentrations vs Altitude for an Anemic with cstar = 0.5*cref')
+legend('cAbar - mean alveolar concentration of oxygen', 'cabar - mean arterial concentration of oxygen', 'cv - venous concentration of oxygen')
